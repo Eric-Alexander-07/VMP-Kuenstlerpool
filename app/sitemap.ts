c45f56f48e4cae73c380e3417a31ecdac8cde690
Eric <eschuck58@gmail.com>
2026-06-01 07:46:58 +0200

    dynamic Sitemap fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from 'next'
 import { getBandsForNav } from '@/lib/bands'
 
+export const dynamic = 'force-dynamic'
+
 const BASE_URL = 'https://v-m-p.com'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
